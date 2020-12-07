fa11d57ffc1f58b16770deeeee4f922c082c1d14
Sorin <sorin19981@gmail.com>
2020-12-07 12:32:09 +0200

    fixed: logged users cant go to login

--- src/main/java/com/endava/booksharing/controller/RegisterController.java
+++ src/main/java/com/endava/booksharing/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package com.endava.booksharing.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +12,11 @@ public class RegisterController {
 
     @GetMapping("/register")
     public ModelAndView registerUser() {
-        return new ModelAndView("register");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            return new ModelAndView("register");
+        }
+        return new ModelAndView("redirect:/all-books");
     }
 }
--- src/test/java/com/endava/booksharing/controller/RegisterControllerTest.java
+++ src/test/java/com/endava/booksharing/controller/RegisterControllerTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -23,6 +26,9 @@ class RegisterControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private Authentication auth;
 
     @MockBean
     private UserDetailsServiceImpl userDetailsService;
@@ -37,4 +43,12 @@ class RegisterControllerTest {
                 .andExpect(view().name("register"));
     }
 
+    @Test
+    @WithMockUser(authorities = "USER")
+    void shouldReturnAllBooksOnLoggedUser() throws Exception {
+
+        mockMvc.perform(get("/register"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/all-books"));
+    }
 }
